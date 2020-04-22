0fedac130ab38d7171ad914db270cbb8d0f48632
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-22 15:16:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
         create_table :costume_stores do |t|
           t.string :name
           t.string :location
-          t.integer :costume_inventory
+          t.string :theme
           t.integer :num_of_employees
           t.boolean :still_in_business
           t.datetime :opening_time
