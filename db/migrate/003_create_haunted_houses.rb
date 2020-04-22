75e95b49d093a68c44e68bf023d78ccf5cf513be
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-22 15:18:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,9 +7,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
           t.string :location
           t.string :theme
           t.integer :price
-          t.boolean :still_in_business
+          t.boolean :family_friendly
           t.datetime :opening_time
           t.datetime :closing_time
+          
        end
     end
 end  
