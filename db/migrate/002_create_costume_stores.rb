a121372dd5f3df9df1cc17544e91abd361f8b760
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-22 13:41:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
           t.integer :costume_inventory
           t.integer :num_of_employees
           t.boolean :still_in_business
-          t.timestamps
+          t.datetime :ooening_time
         end
       end
 end  
